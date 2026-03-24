b797818a386ed9297b96f1c424b8d208020719a6
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-23 23:06:39 -0500

    basic styling grimore homepage

--- src/components/Parts/BackButton.tsx
+++ src/components/Parts/BackButton.tsx
@@ -3,8 +3,7 @@ import { useNavigate } from 'react-router-dom'
 export default function BackButton({ name }: { name: any }) {
   const navigate = useNavigate();
   return (
-    <button className="nav-button"
-      style={{ backgroundColor: "#9e38fd" }}
+    <button className="navbutton"
       onClick={() => navigate(-1)}>
       { name }
     </button>
