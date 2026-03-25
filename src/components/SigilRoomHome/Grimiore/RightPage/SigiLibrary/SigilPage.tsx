9ef83e4989e7f8ffd778e4bc28869ef7cfd21bdb
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-25 16:19:02 -0500

    add sigilpage styling, dummy sigil

--- src/components/SigilRoomHome/Grimiore/RightPage/SigiLibrary/SigilPage.tsx
+++ src/components/SigilRoomHome/Grimiore/RightPage/SigiLibrary/SigilPage.tsx
@@ -6,15 +6,29 @@ export default function SigilPage( ) {
     const {state} = useLocation();
     const {sigilKey, sigilData} = state;
     console.log(sigilKey, sigilData)
-  return (<div>
-    <h1>This is the SigilPage for </h1>
+  return (<div className="sigilpage">
+    <h1>This is the SigilPage for {sigilData.name}</h1>
     <br />
+
+    <div className="sigildetails">
+
+    {sigilData.name}
+        <br />
+        was created on : {sigilData.created_at}
+        <br />
+    at {sigilData.location.name}
+    <br/>
+    {sigilData.isCharged ? "Sigil is Charged" : "Sigil is not Charged"}
+        <br />
+    </div>
+    
     <Link to="/charge-sigil">Charge Sigil</Link>
     <br />
     <Link to="/destroy-sigil">Destroy Sigil</Link>
     <br />
-
+    <img className="sigilbox"src="src/assets/dummySigil.svg"/>
     <br />
     <BackButton name={"Go Back"}/>
+
   </div>)
 };
--- src/components/SigilRoomHome/Grimiore/RightPage/SigiLibrary/SigilThumb.tsx
+++ src/components/SigilRoomHome/Grimiore/RightPage/SigiLibrary/SigilThumb.tsx
@@ -1,9 +1,8 @@
 import { Link } from 'react-router-dom'
 
 export default function SigilThumb({ sigilKey, sigilData }: { sigilKey: string, sigilData: any }) {
-  console.log(sigilData)
   return (
-    <Link className="sigilpage" to="/sigil-page" state={{ sigilKey, sigilData }}>
+    <Link className="sigilthumb" to="/sigil-page" state={{ sigilKey, sigilData }}>
       <div>
         <p>{sigilData.name}</p>
         <p>{sigilData.location.name}</p>
