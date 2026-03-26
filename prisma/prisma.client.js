3a1b2ff6b1da816874df98906d68002aab6563b3
HopeyClarkey <thehopeclarke@gmail.com>
2026-03-26 16:56:17 -0500

    db prisma config set up

--- prisma/prisma.client.js
+++ prisma/prisma.client.js
@@ -2,11 +2,15 @@ import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
 
-const adapter = new PrismaClient({
-  adapter: new PrismaMariaDb({
-    connectionString: process.env.DATABASE_URL,
-  }),
-});
+const url = new URL(process.env.DATABASE_URL)
 
+const adapter = new PrismaMariaDb({
+  host: url.hostname,
+  user: url.username,
+  password: url.password || undefined,
+  database: url.pathname.slice(1),
+  port: url.port ? parseInt(url.port) : 3306,
+  connectionLimit: 5,
+});
 const prisma = new PrismaClient({ adapter });
 export default prisma;
